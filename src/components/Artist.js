7f1230803a292866752cbdbac57af2302d1e0c72
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-10-16 17:02:51 +0100

    some front end functionality

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import '../CSS/artists.css'
 import { Zoom } from 'react-slideshow-image'
 
 const Artist = ({ artist, comics }) => {
-  const images = artist.comics.map(comic => comic.image)
+  const images = artist.comics.map(comic => comic)
+
+  const renderComics = () => {
+    return images.map(comic => {
+      return (
+        <Link to={`/comics/${comic.id}`}>
+          <img key={comic.id} src={comic.image} alt='' />
+          <h2>{comic.name}</h2>
+        </Link>
+      )
+    })
+  }
 
   const zoomOutProperties = {
     duration: 8000,
@@ -19,9 +31,7 @@ const Artist = ({ artist, comics }) => {
     <div className='artist-card'>
       <h1>{artist.name}</h1>
       <Zoom className='zoom-component' {...zoomOutProperties}>
-        {images.map((each, index) => (
-          <img key={index} src={each} alt='' />
-        ))}
+        {renderComics()}
       </Zoom>
     </div>
   )
--- src/containers/ComicsContainer.js
+++ src/containers/ComicsContainer.js
@@ -12,12 +12,33 @@ class ComicsContainer extends React.Component {
     this.props.getComics()
   }
 
+  shuffle (comics = []) {
+    let currentIndex = comics.length
+
+    let temporaryValue
+
+    let randomIndex
+
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex -= 1
+      // And swap it with the current element.
+      temporaryValue = comics[currentIndex]
+      comics[currentIndex] = comics[randomIndex]
+      comics[randomIndex] = temporaryValue
+    }
+
+    return comics
+  }
+
   render () {
     return (
       <>
         <Nav />
         <div className='comics-container'>
-          {this.props.comics.map(comic => (
+          {this.shuffle(this.props.comics).map(comic => (
             <Comic key={comic.complete_comic.id} comic={comic.complete_comic} />
           ))}
         </div>
